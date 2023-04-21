479a1f6c5fe693b51e23b6f39e0f4c1f40ac82fa
Marta Wojcik <marta.bus@onet.pl>
2023-04-21 10:36:25 +0200

    comiting old things again???

---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+  {
+    short: "EUR",
+    name: "euro",
+    rate: 4.69,
+  },
+  {
+    short: "GBR",
+    name: "funt brytyjski",
+    rate: 5.33,
+  },
+  {
+    short: "USD",
+    name: "dolar amerykański",
+    rate: 4.41,
+  },
+  {
+    short: "NOK",
+    name: "korona norweska",
+    rate: 0.45,
+  },
+];
