b71bb556cadaab3ed9b550e4a0fdeed0bb96c091
Marta Wojcik <marta.bus@onet.pl>
2023-04-25 10:39:01 +0200

    shortened code,

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -2,9 +2,9 @@ import "./style.css";
 
 export const Result = ({ result }) => (
   <p className="result">
-    {result !== undefined && (
+    {result && (
       <>
-        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
         <strong>
           {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
         </strong>
