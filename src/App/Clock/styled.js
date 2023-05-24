25138dc5ac2ad49d9d8e667072d404ec5e05180c
Marta Wojcik <marta.bus@onet.pl>
2023-05-24 10:46:57 +0200

    styled.js created in Clock

---
+++ src/App/Clock/styled.js
@@ -0,0 +1,7 @@
+import styled, { css } from "styled-components";
+
+export const StyledClock = styled.div`
+  text-align: right;
+  font-family: monospace;
+  color: ${({ theme }) => theme.color.boulder};
+`;
