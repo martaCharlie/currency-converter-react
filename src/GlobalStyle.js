dc376ab062b87cb4bafd0cadf6e406b2ddcb7dc8
Marta Wojcik <marta.bus@onet.pl>
2023-05-24 10:42:30 +0200

    GlobalStyle.js created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./money3.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+#root {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-position: center;
+}
+`;
