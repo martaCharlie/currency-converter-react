18664d24216d7ace4499da4ad574d4cfb284b6fb
Marta Wojcik <marta.bus@onet.pl>
2023-05-27 14:06:10 +0200

    padding to CLOCK added

--- src/App/Clock/styled.js
+++ src/App/Clock/styled.js
@@ -1,7 +1,9 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const StyledClock = styled.div`
   text-align: right;
   font-family: monospace;
   color: ${({ theme }) => theme.color.boulder};
+  padding-top: 10px;
+  padding-right: 10px;
 `;
